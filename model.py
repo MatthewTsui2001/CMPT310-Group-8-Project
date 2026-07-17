be539409474d8a4b75cbc434def922ef9f5c0b3d
Cat <91161517+4CBanana@users.noreply.github.com>
2026-07-16 20:07:12 -0700

    add model.py

--- model.py
+++ model.py
@@ -1,21 +1,54 @@
 import tensorflow as tf
-from tensorflow.keras import layers, Sequential
+from tensorflow.keras import layers, Model
 
-IMG_HEIGHT = 32
-IMG_WIDTH = 32
+IMG_HEIGHT = 128
+IMG_WIDTH = 128
+NUM_BREEDS = 120    # Stanford dataset = 120 dog breeds
+NUM_AGE_GROUPS = 3  # DogAge dataset = 3 age categories (Young, Adult, Senior)
 
-model = Sequential([
-    layers.Rescaling(1./255, input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
+def multitask_model():
+    #placeholder for input
+    input = layers.Input(shape = (IMG_HEIGHT, IMG_WIDTH, 3), name = "input_image")
 
-    layers.Conv2D(32, (3, 3), activation='relu'),
-    layers.MaxPooling2D((2, 2)),
+    #rescaling input pixel values
+    x = layers.Rescaling(1./255)(input)
 
-    layers.Conv2D(64, (3, 3), activation='relu'),
-    layers.MaxPooling2D((2, 2)),
+    #first convolution
+    x = layers.Conv2D(32, (3, 3), padding = 'same', activation = 'relu')(x)
+    #normalization
+    x = layers.BatchNormalization()(x)
+    #extract dominant features
+    x = layers.MaxPooling2D((2, 2))(x)
 
-    layers.GlobalAveragePooling2D(),
+    #second convolution
+    x = layers.Conv2D(64, (3, 3), padding = 'same', activation = 'relu')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.MaxPooling2D((2, 2))(x)
 
-    layers.Dense(64, activation='relu'),
-    layers.Dropout(0.5),
-    layers.Dense(1, activation='sigmoid')
-])
+    #third convolution
+    x = layers.Conv2D(128, (3, 3), padding = 'same', activation = 'relu')(x)
+    x = layers.BatchNormalization()(x)
+    x = layers.MaxPooling2D((2, 2))(x)
+
+    #average each layer value
+    features = layers.GlobalAveragePooling2D()(x)
+
+    #---------Multitask Learning---------
+
+    #breed classification branch
+    #apply neurons
+    breed_features = layers.Dense(128, activation = 'relu')(features)
+    #drop neurons (reduce overfit)
+    breed_features = layers.Dropout(0.3)(breed_features)
+    #apply neurons, softmax assigns probability of breed type
+    breed_output = layers.Dense(NUM_BREEDS, activation = 'softmax', name = 'breed_output')(breed_features)
+
+    #age classification branch
+    age_features = layers.Dense(128, activation = 'relu')(features)
+    age_features = layers.Dropout(0.3)(age_features)
+    age_output = layers.Dense(NUM_AGE_GROUPS, activation = 'softmax', name = 'age_output')(age_features)
+
+    model = Model(inputs=input, outputs=[breed_output, age_output])
+    return model
+
+model = multitask_model()
